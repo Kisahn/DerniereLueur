2e9f5d5f815afa5ae2bd12f912dd5576fc25c617
Kisahn <jdeck.dw@gmail.com>
2025-04-28 14:02:29 +0200

    Initial import
    Original version of the project. Game works well

---
+++ DerniereLueur_WwiseProject/Originals/Wwise_IDs.h
@@ -0,0 +1,105 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBIENT = 1562304622U;
+        static const AkUniqueID PLAY_DETECTEUR = 1610494137U;
+        static const AkUniqueID PLAY_DIGICODELOOSE = 653889250U;
+        static const AkUniqueID PLAY_DIGICODEWIN_01 = 2329151100U;
+        static const AkUniqueID PLAY_DOUBLEPORTE = 1653130081U;
+        static const AkUniqueID PLAY_ENDDOOR = 3802672865U;
+        static const AkUniqueID PLAY_FERMETURETRAPPE = 3352877353U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_FOOTSTEPS_SURFACE = 2872098335U;
+        static const AkUniqueID PLAY_FOOTSTEPSV2 = 3068712267U;
+        static const AkUniqueID PLAY_GARE = 367638521U;
+        static const AkUniqueID PLAY_HOVER_TRY3 = 295075901U;
+        static const AkUniqueID PLAY_IDLE_ = 796324511U;
+        static const AkUniqueID PLAY_JUMP = 3689126666U;
+        static const AkUniqueID PLAY_LENTILLE = 3486787651U;
+        static const AkUniqueID PLAY_LIGHT = 456848542U;
+        static const AkUniqueID PLAY_LIGHTGARE = 2094960357U;
+        static const AkUniqueID PLAY_LOUPIOTTETHEME = 1005975674U;
+        static const AkUniqueID PLAY_MEMORIES = 3542492639U;
+        static const AkUniqueID PLAY_MENU = 1278378707U;
+        static const AkUniqueID PLAY_PORTE2 = 2491993342U;
+        static const AkUniqueID PLAY_PORTECIMETIERE = 3485854207U;
+        static const AkUniqueID PLAY_PORTEGARE = 2736630979U;
+        static const AkUniqueID PLAY_RAIL = 2838936950U;
+        static const AkUniqueID PLAY_RECEPTOR_TRY3 = 3630471467U;
+        static const AkUniqueID PLAY_RESISTANCE = 4261948093U;
+        static const AkUniqueID PLAY_SOLHORLOGE = 938198704U;
+        static const AkUniqueID PLAY_TRASH = 264656916U;
+        static const AkUniqueID PLAY_TRUERECEPTEUR = 1847364307U;
+        static const AkUniqueID PLAY_VALID_TRY3 = 1347300029U;
+        static const AkUniqueID PLAY_WAGTOOLV3 = 4165707282U;
+        static const AkUniqueID PLAY_WIND = 1020223172U;
+        static const AkUniqueID STOP_AMBIENTPLAYRESIST = 3811473472U;
+        static const AkUniqueID STOP_DOUBLEPORTE = 236069363U;
+        static const AkUniqueID STOP_GARE = 4186807575U;
+        static const AkUniqueID STOP_LIGHT = 3054153748U;
+        static const AkUniqueID STOP_LOUPIOTTETHEME_INTO_RESISTANCE = 733188579U;
+        static const AkUniqueID STOP_MENU = 2914981333U;
+        static const AkUniqueID STOP_PORTE2 = 185870704U;
+        static const AkUniqueID STOP_PORTECIMETIERE = 4052960717U;
+        static const AkUniqueID STOP_PORTEGARE = 1832324221U;
+        static const AkUniqueID STOP_RAIL = 3206237488U;
+        static const AkUniqueID STOP_RESISTANCE = 2415374655U;
+        static const AkUniqueID STOP_RESISTANCE_INTO_THEME = 3002768438U;
+        static const AkUniqueID STOP_WAGTOOL = 863913257U;
+        static const AkUniqueID STOP_WAGTOOLV3 = 1578277748U;
+        static const AkUniqueID STOP_WIND = 3173136834U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SWT_SURFACE_TYPE_ANIM
+        {
+            static const AkUniqueID GROUP = 744792908U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID PONTS = 2311298919U;
+                static const AkUniqueID WATER = 2654748154U;
+            } // namespace SWITCH
+        } // namespace SWT_SURFACE_TYPE_ANIM
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CHARACTER = 436743010U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID CHARACTER = 436743010U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
